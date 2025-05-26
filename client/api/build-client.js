945353c5bad2f300738e1b3ba4fc2909a8e15c9a
TriDecent <116976929+TriDecent@users.noreply.github.com>
2025-05-26 17:19:44 +0700

    update server base URL in build-client.js to use traefik service

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -16,7 +16,7 @@ export default ({ req }) => {
     // we are on the server!
     return axios.create({
       baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+        'http://traefik.kube-system.svc.cluster.local',
       headers: req.headers,
     });
   } else {
